9b6c41d67d59f8a7fbce9d8e56a8417fdcbc0a55
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-10-10 20:56:34 +0500

    code

--- app/components/Categories/Categories.jsx
+++ app/components/Categories/Categories.jsx
@@ -7,21 +7,21 @@ import Image from 'next/image'
 export default function Categories() {
     return (
         <ul className='flex-col text-black font-medium font-sans border-r pr-10 '>
-            <div className='flex items-center mt-8'>
+            <div className='flex items-center mt-8 cursor-pointer'>
                 <span className='flex-1 whitespace-nowrap'>Woman’s Fashion</span>
                 <Image src={DropDown} width={25} height={25} objectPosition='contain' className='ml-7' />
             </div>
-            <div className='flex items-center mt-4'>
+            <div className='flex items-center mt-4 cursor-pointer'>
                 <span className='flex-1 whitespace-nowrap'>Men’s Fashion</span>
                 <Image src={DropDown} width={25} height={25} objectPosition='contain' className='ml-14' />
             </div>
 
-            <li className='mt-4'>Electronics</li>
-            <li className='mt-4' >Home & Lifestyle</li>
-            <li className='mt-4'>Medicine</li>
-            <li className='mt-4'>Sports & Outdoor</li>
-            <li className='mt-4'>Baby’s & Toys</li>
-            <li className='mt-4'>Health & Beauty</li>
+            <li className='mt-4 cursor-pointer'>Electronics</li>
+            <li className='mt-4 cursor-pointer' >Home & Lifestyle</li>
+            <li className='mt-4 cursor-pointer'>Medicine</li>
+            <li className='mt-4 cursor-pointer'>Sports & Outdoor</li>
+            <li className='mt-4 cursor-pointer'>Baby’s & Toys</li>
+            <li className='mt-4 cursor-pointer'>Health & Beauty</li>
         </ul>
     )
 }
